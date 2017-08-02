a097004c0b51daeb7694ee455e9098f833d3dbdb
lwyq <lynn.wongyuqing@gmail.com>
2017-08-02 17:55:49 +0800

    added user_event pages

---
+++ client/app/user_event/brief/user_eventbrief.controller.js
@@ -0,0 +1,12 @@
+(function () {
+    angular
+        .module("DMS")          
+        .controller("UserEventBriefCtrl", UserEventBriefCtrl);    
+
+    function UserEventBriefCtrl() {
+
+        var vm = this;
+
+    } // END UserEventBriefCtrl
+
+})();
