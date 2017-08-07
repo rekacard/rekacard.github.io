2558fa6bf34bc4d851ba12c5bce09e9ed31a0d63
rekacard <mtwong2005@yahoo.com.sg>
2017-08-07 22:30:16 +0800

    Modify client route to resolve Passport auth before view is display.

--- client/app/config/app.route.js
+++ client/app/config/app.route.js
@@ -130,72 +130,234 @@
             controller: 'PastEventCtrl as ctrl',
           }
         },
-        // resolve: {
-        //   user: function(PassportSvc) {
-        //     return PassportSvc.userAuth()
-        //       .then(function(result) {
-        //         return result.data.user;
-        //       })
-        //       .catch(function(err) {
-        //         return '';
-        //       });
-        //   }
-        // },
+        resolve: {
+          user: function(PassportSvc) {
+            return PassportSvc.userAuth()
+              .then(function(result) {
+                return result.data.user;
+              })
+              .catch(function(err) {
+                return '';
+              });
+          }
+        },
       })
-            .state("user_eventsignup", {
-                    url: "/user_eventsignup",
-                    templateUrl: "app/user_event/signup/user_eventsignup.html",
-                    controller: "UserEventSignupCtrl",
-                    controllerAs: "ctrl"
-            })
-            .state("user_eventedit", {
-                    url: "/user_eventedit",
-                    templateUrl: "app/user_event/eventedit/user_eventedit.html",
-                    controller: "UserEventEditCtrl",
-                    controllerAs: "ctrl"
-            })
-            .state("user_eventdetails", {
-                    url: "/user_eventdetails",
-                    templateUrl: "app/user_event/eventdetails/user_eventdetails.html",
-                    controller: "UserEventDetailsCtrl",
-                    controllerAs: "ctrl"
-            })
-            .state('user_eventsubmission', {
-                    url:'/user_eventsubmission',
-                    templateUrl: 'app/user_event/submission/user_eventsubmission.html',
-                    controller: "UserEventSubmissionCtrl",
-                    controllerAs: "ctrl"
-            })
-            .state('user_eventbrief', {
-                    url:'/user_eventbrief',
-                    templateUrl: 'app/user_event/brief/user_eventbrief.html',
-                    controller: "UserEventBriefCtrl",
-                    controllerAs: "ctrl"
-            })
-            .state('user_eventfeedback', {
-                    url:'/user_eventfeedback',
-                    templateUrl: 'app/user_event/feedback/user_eventfeedback.html',
-                    controller: "UserEventFeedbackCtrl",
-                    controllerAs: "ctrl"
-            })
-            .state('user_eventattendance', {
-                    url:'/user_eventattendance',
-                    templateUrl: 'app/user_event/attendance/user_eventattendance.html',
-                    controller: "UserEventAttendanceCtrl",
-                    controllerAs: "ctrl"
-            })
-            .state('user_eventdebrief', {
-                    url:'/user_eventdebrief',
-                    templateUrl: 'app/user_event/attendance/user_eventdebrief.html',
-                    controller: "UserEventDebriefCtrl",
-                    controllerAs: "ctrl"
-            })
-            .state('analytics_userdetails', {
-                    url:'/analytics_userdetails',
-                    templateUrl: 'app/analytics/analytics_userdetails/analytics_userdetails.html',
-                    controller: "AnalyticsUserDetailsCtrl",
-                    controllerAs: "ctrl"
-            })
+      .state("user_eventsignup", {
+        url: "/api/user_eventsignup",
+        views: {
+          'menu': {
+            templateUrl: 'app/menu/menu.html',
+            controller: 'MenuCtrl as ctrl',
+          },
+          'content': {
+            templateUrl: 'app/user_event/signup/user_eventsignup.html',
+            controller: 'UserEventSignupCtrl as ctrl',
+          }
+        },
+        resolve: {
+          user: function(PassportSvc) {
+            return PassportSvc.userAuth()
+              .then(function(result) {
+                return result.data.user;
+              })
+              .catch(function(err) {
+                return '';
+              });
+          }
+        },
+      })
+      .state("user_eventedit", {
+        url: "/api/user_eventedit",
+        views: {
+          'menu': {
+            templateUrl: 'app/menu/menu.html',
+            controller: 'MenuCtrl as ctrl',
+          },
+          'content': {
+            templateUrl: 'app/user_event/eventedit/user_eventedit.html',
+            controller: 'UserEventEditCtrl as ctrl',
+          }
+        },
+        resolve: {
+          user: function(PassportSvc) {
+            return PassportSvc.userAuth()
+              .then(function(result) {
+                return result.data.user;
+              })
+              .catch(function(err) {
+                return '';
+              });
+          }
+        },
+      })
+      .state("user_eventdetails", {
+        url: "/api/user_eventdetails",
+        views: {
+          'menu': {
+            templateUrl: 'app/menu/menu.html',
+            controller: 'MenuCtrl as ctrl',
+          },
+          'content': {
+            templateUrl: 'app/user_event/eventdetails/user_eventdetails.html',
+            controller: 'UserEventDetailsCtrl as ctrl',
+          }
+        },
+        resolve: {
+          user: function(PassportSvc) {
+            return PassportSvc.userAuth()
+              .then(function(result) {
+                return result.data.user;
+              })
+              .catch(function(err) {
+                return '';
+              });
+          }
+        },
+      })
+      .state('user_eventsubmission', {
+        url:'/api/user_eventsubmission',
+        views: {
+          'menu': {
+            templateUrl: 'app/menu/menu.html',
+            controller: 'MenuCtrl as ctrl',
+          },
+          'content': {
+            templateUrl: 'app/user_event/submission/user_eventsubmission.html',
+            controller: 'UserEventSubmissionCtrl as ctrl',
+          }
+        },
+        resolve: {
+          user: function(PassportSvc) {
+            return PassportSvc.userAuth()
+              .then(function(result) {
+                return result.data.user;
+              })
+              .catch(function(err) {
+                return '';
+              });
+          }
+        },
+      })
+      .state('user_eventbrief', {
+        url:'/api/user_eventbrief',
+        views: {
+          'menu': {
+            templateUrl: 'app/menu/menu.html',
+            controller: 'MenuCtrl as ctrl',
+          },
+          'content': {
+            templateUrl: 'app/user_event/brief/user_eventbrief.html',
+            controller: 'UserEventBriefCtrl as ctrl',
+          }
+        },
+        resolve: {
+          user: function(PassportSvc) {
+            return PassportSvc.userAuth()
+              .then(function(result) {
+                return result.data.user;
+              })
+              .catch(function(err) {
+                return '';
+              });
+          }
+        },
+      })
+      .state('user_eventfeedback', {
+        url:'/api/user_eventfeedback',
+        views: {
+          'menu': {
+            templateUrl: 'app/menu/menu.html',
+            controller: 'MenuCtrl as ctrl',
+          },
+          'content': {
+            templateUrl: 'app/user_event/feedback/user_eventfeedback.html',
+            controller: 'UserEventFeedbackCtrl as ctrl',
+          }
+        },
+        resolve: {
+          user: function(PassportSvc) {
+            return PassportSvc.userAuth()
+              .then(function(result) {
+                return result.data.user;
+              })
+              .catch(function(err) {
+                return '';
+              });
+          }
+        },
+      })
+      .state('user_eventattendance', {
+        url:'/api/user_eventattendance',
+        views: {
+          'menu': {
+            templateUrl: 'app/menu/menu.html',
+            controller: 'MenuCtrl as ctrl',
+          },
+          'content': {
+            templateUrl: 'app/user_event/attendance/user_eventattendance.html',
+            controller: 'UserEventAttendanceCtrl as ctrl',
+          }
+        },
+        resolve: {
+          user: function(PassportSvc) {
+            return PassportSvc.userAuth()
+              .then(function(result) {
+                return result.data.user;
+              })
+              .catch(function(err) {
+                return '';
+              });
+          }
+        },
+      })
+      .state('user_eventdebrief', {
+        url:'/api/user_eventdebrief',
+        views: {
+          'menu': {
+            templateUrl: 'app/menu/menu.html',
+            controller: 'MenuCtrl as ctrl',
+          },
+          'content': {
+            templateUrl: 'app/user_event/attendance/user_eventdebrief.html',
+            controller: 'UserEventDebriefCtrl as ctrl',
+          }
+        },
+        resolve: {
+          user: function(PassportSvc) {
+            return PassportSvc.userAuth()
+              .then(function(result) {
+                return result.data.user;
+              })
+              .catch(function(err) {
+                return '';
+              });
+          }
+        },
+      })
+      .state('analytics_userdetails', {
+        url:'/api/analytics_userdetails',
+        views: {
+          'menu': {
+            templateUrl: 'app/menu/menu.html',
+            controller: 'MenuCtrl as ctrl',
+          },
+          'content': {
+            templateUrl: 'app/analytics/analytics_userdetails/analytics_userdetails.html',
+            controller: 'AnalyticsUserDetailsCtrl as ctrl',
+          }
+        },
+        resolve: {
+          user: function(PassportSvc) {
+            return PassportSvc.userAuth()
+              .then(function(result) {
+                return result.data.user;
+              })
+              .catch(function(err) {
+                return '';
+              });
+          }
+        },
+      })
       ;
 
     $urlRouterProvider.otherwise("/home");
