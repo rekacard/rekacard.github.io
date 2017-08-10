ca03993505aa5bd75b57b189c7a09794699f5a28
lwyq <lynn.wongyuqing@gmail.com>
2017-08-10 14:46:30 +0800

    added analytics usersearch

---
+++ client/app/analytics/analytics_usersearch/analytics_usersearch.controller.js
@@ -0,0 +1,12 @@
+(function () {
+    angular
+        .module("PAF")          
+        .controller("AnalyticsUserSearchCtrl", AnalyticsUserSearchCtrl);    
+
+    function AnalyticsUserSearchCtrl() {
+
+        var vm = this;
+
+    } // END AnalyticsUserSearchCtrl
+
+})();
