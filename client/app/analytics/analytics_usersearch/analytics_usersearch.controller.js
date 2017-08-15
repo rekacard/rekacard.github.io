81c24642e9e3d69600d542c796cdf99481f9a4da
rekacard <mtwong2005@yahoo.com.sg>
2017-08-15 22:28:51 +0800

    Allow page to be view only if user is authenticated.

--- client/app/analytics/analytics_usersearch/analytics_usersearch.controller.js
+++ client/app/analytics/analytics_usersearch/analytics_usersearch.controller.js
@@ -3,9 +3,16 @@
         .module("PAF")          
         .controller("AnalyticsUserSearchCtrl", AnalyticsUserSearchCtrl);    
 
-    function AnalyticsUserSearchCtrl() {
+    AnalyticsUserSearchCtrl.$inject = [ 'user' ];
+
+    function AnalyticsUserSearchCtrl(user) {
 
         var vm = this;
+        if (user) {
+            vm.parseuser = user;
+            vm.user = vm.parseuser.split(',')[0];
+            vm.role = (vm.parseuser.split(',')[1] == '1')? '1':'';
+        }
 
     } // END AnalyticsUserSearchCtrl
 
